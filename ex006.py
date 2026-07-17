9001761cd1615b5a7104705217f26cc2b1fb37e0
Cleiton Barreto Pereira <cleitonbarretopereira@outlook.com>
2026-07-17 14:51:56 -0300

    Ex006
    Dobro, triplo e raiz quadrada

---
+++ ex006.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número: '))
+print('O DOBRO de {} é {}'.format(num, num*2))
+print('O TRIPLO de {} é {}'.format(num, num * 3))
+print('A RAIZ QUADRADA de {} é {}'.format(num, num**0.5))
