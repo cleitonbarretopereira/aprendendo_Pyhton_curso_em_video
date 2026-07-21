59cf021833965f8bc09359fe9d5129c7772a1daf
Cleiton Barreto Pereira <cleitonbarretopereira@outlook.com>
2026-07-21 14:17:38 -0300

    Criar ex007

---
+++ ex007.py
@@ -0,0 +1,4 @@
+print('CÁLCULO DE MÉDIA DA PROVA')
+nota1 = float(input('Digite a nota da primeira prova: ' ))
+nota2 = float(input('Digite a nota da segunda prova: '))
+print('A MÉDIA do aluno no período foi {}.'.format((nota1+nota2)/2))
