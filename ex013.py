dab144c42dd45421b4f757a674570127cdd15841
Cleiton Barreto Pereira <cleitonbarretopereira@outlook.com>
2026-07-23 13:49:12 -0300

    REAJUSTE DE SALÁRIO
    ex013

---
+++ ex013.py
@@ -0,0 +1,4 @@
+print('REAJUSTE DE SALÁRIO')
+salario = float(input('Digite seu salário: '))
+aumento = 0.15
+print('Você recebeu um aumento de R$ {:.2f}, seu novo salário é R$ {:.2f}.'.format(salario*aumento,salario*aumento+salario))
