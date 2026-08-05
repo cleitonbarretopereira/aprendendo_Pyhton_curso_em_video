94403333daec0a5939ff7db44bea89ae98f5ee64
Cleiton Barreto Pereira <cleitonbarretopereira@outlook.com>
2026-08-05 13:51:36 -0300

    ANALISANDO NOME
    ex022

---
+++ ex022.py
@@ -0,0 +1,8 @@
+print('ANALISANDO NOME')
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome em maaiusculo é {}'.format(nome.upper()))
+print('Seu nome em minúsculo é {} .'.format(nome.lower()))
+print('Seu nome tem {} letras.'.format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras.'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e contém {} letras.'.format(separa[0], len(separa[0])))
