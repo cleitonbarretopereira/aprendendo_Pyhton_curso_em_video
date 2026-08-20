7fee9683aae1b7a6b2878870fe93c85687983fe7
Cleiton Barreto Pereira <cleitonbarretopereira@outlook.com>
2026-08-20 13:39:45 -0300

    Jogo de Adivinhação

--- ex028.py
+++ ex028.py
@@ -2,12 +2,12 @@ from random import randint
 from time import sleep
 
 print('JOGO DE ADIVINHAÇÃO')
-
+print('*--'*18)
 comp = randint(1,10)
 usuario = int(input('Pensei em um número de 1 a 10, adivinhe qual é: '))
-
+print('Pensando...')
 sleep(2)
-
+print('*--'*18)
 if comp==usuario:
     print('Parabéns, vc acertou, também pensei no {}.'.format(usuario))
 else:
